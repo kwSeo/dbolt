1be97093975d3ef54ef566efc2e545aafa2cd230
Kyeongwon Seo <ruddnjs1230@gmail.com>
2023-02-05 16:41:59 +0900

    httpkv 구현체 추가

---
+++ pkg/dbolt/httpkv/model.go
@@ -0,0 +1,12 @@
+package httpkv
+
+type GetReq struct {
+	BucketName []byte `json:"bucketName"`
+	Key        []byte `json:"key"`
+}
+
+type PutReq struct {
+	BucketName []byte `json:"bucketName"`
+	Key        []byte `json:"key"`
+	Value      []byte `json:"value"`
+}
